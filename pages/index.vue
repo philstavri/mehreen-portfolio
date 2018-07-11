9db30c98dd1d7fb958fb1fd916dd04d8698a7e2c
philstavri <2485485+philstavri@users.noreply.github.com>
2018-07-11 15:36:10 +0100

    Add enjoy feature to about page

--- pages/index.vue
+++ pages/index.vue
@@ -9,12 +9,28 @@
             I'm Mehreen Stavri
           </h2>
           <p>Senior User Experience Designer / Information Architect with over 10 years’ experience in UX across multiple industries, including transport, financial services, telecom and government.</p>
-          <p class="u-margin-top-l">My experience covers a range of responsibilities, including facilitating workshops, capturing requirements, producing user journeys, and creating high-fidelity, interactive wireframes for mobile, tablet and desktop.
-      I have extensive experience of working on large-scale projects within multi-disciplinary teams and value the importance of each role at all stages of a project.</p>
+          <p class="u-margin-top-l">My experience covers a range of responsibilities, including facilitating workshops, capturing requirements, producing user journeys, and creating high-fidelity, interactive wireframes for mobile, tablet and desktop.</p>
+          <p class="u-margin-top-l">I have extensive experience of working on large-scale projects within multi-disciplinary teams and value the importance of each role at all stages of a project.</p>
 
           <hr/>
 
           <h2 class="subtitle">I enjoy...</h2>
+
+          <div class="enjoy-feature">
+            <div class="enjoy-feature__column">
+              <h3 class="enjoy-feature__column-title">Exploring</h3>
+              <p class="enjoy-feature__detail">Lorem ipsum dolor sizzle amet, consectetuer adipiscing my shizz. We gonna chung dope velizzle, the bizzle volutpat, suscipizzle quizzle, gravida funky fresh, i saw beyonces tizzles and my pizzle went crizzle.</p>
+            </div>
+            <div class="enjoy-feature__column">
+              <h3 class="enjoy-feature__column-title">Analysing</h3>
+              <p class="enjoy-feature__detail">Pellentesque izzle tortor. Sed bow wow wow. Cool izzle yo dapibus phat tempizzle tempor. Hizzle pellentesque nibh izzle shut the shizzle up. Vestibulum check it out tortizzle. Mofo gizzle that's the shizzle doggy. </p>
+            </div>
+            <div class="enjoy-feature__column">
+              <h3 class="enjoy-feature__column-title">Creating</h3>
+              <p class="enjoy-feature__detail"> In hac habitasse platea dictumst. Yippiyo dapibizzle. Curabitur tellus gangsta, pretium brizzle, mattizzle boom shackalack, eleifend ma nizzle, gangster. Bizzle fo shizzle mah nizzle fo rizzle, mah home g-dizzle. Integizzle sempizzle velit gangster dope.</p>
+            </div>
+          </div>
+
         </div>
         <div slot="second-col">&nbsp;</div>
       </two-col>
@@ -40,10 +56,12 @@ export default {
   background-repeat: no-repeat;
   background-position: left 50vw center;
   background-size: cover;
+  padding-bottom: 100px;
 
   &__content {
     @include full-width-content;
     height: 100%;
+    @include spacing(margin-bottom, xxxl);
   }
 
   @media (max-width: 700px) {
@@ -56,6 +74,27 @@ export default {
     &__content {
       background-color: $color-shade-light;
     }
+  }
+}
+
+.enjoy-feature {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+
+  &__column {
+    flex: 1 0 33.3333%;
+    min-width: 175px;
+    @include spacing(padding-left padding-right, xs);
+    @include spacing(margin-bottom, xxl);
+  }
+
+  &__column-title {
+    font-weight: 300;
+  }
+
+  &__detail {
+    @include spacing(margin-top, s);
   }
 }
 
