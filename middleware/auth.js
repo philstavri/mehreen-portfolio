f59c2c746c4189f2ab19f808b68b06c26dfaac42
philstavri <2485485+philstavri@users.noreply.github.com>
2018-07-25 15:47:57 +0100

    Remove console log lines from auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,5 @@
 export default function({redirect, route = {}, store}) {
-  console.log("here");
   if(!store.getters.canAccessWork) {
-    console.log("ROUTE", route);
     store.commit("setAuthSuccessRouteName", route.name);
     return redirect("/authenticate");
   }
